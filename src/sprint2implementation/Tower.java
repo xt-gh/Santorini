bea202905914f3e95c744fc8b9dadb78f9e6abf6
tlau0015 <tlau0015@student.monash.edu>
2025-04-22 22:36:31 +0800

    Refine the Tower class and implement necessary logic in additional methods - Create the addDome method to implement the logic for adding a dome to the top of the tower - Create getLevelCount method to get the height of the tower based on the current number of levels - Create hasDome method to check if the tower has a dome - Create isAtMaxLevel to check if the tower level has reached its maximum number of levels - Implement the logic for getCurrentIcon method to get the tower level's icon and the dome icon

--- src/sprint2implementation/Tower.java
+++ src/sprint2implementation/Tower.java
@@ -26,4 +26,37 @@ public class Tower {
         }
     }
 
+    public void addDome() {
+        if (!hasDome()) {
+            dome = new Dome("src/sprint2implementation/pics/Lvl_complete.png");
+        }
+    }
+
+    public int getLevelCount() {
+        return towerLevels.size();
+    }
+
+    public boolean hasDome() {
+        return dome != null;
+    }
+
+    public boolean isAtMaxLevel() {
+        return towerLevels.size() == MAX_LEVELS;
+    }
+
+    public ImageIcon getCurrentIcon() {
+        if (hasDome())
+        {
+            return dome.getDomeIcon();
+        }
+        else if (!towerLevels.isEmpty())
+        {
+            return towerLevels.get(towerLevels.size() - 1).getLevelIcon();
+        }
+        else
+        {
+            return new ImageIcon("src/sprint2implementation/pics/Empty_tile.png");
+        }
+    }
+
 }
