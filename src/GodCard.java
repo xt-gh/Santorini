73264dec4e5e601601260ec307872462f5768f1d
echi0039 <echi0039@student.monash.edu>
2025-04-23 18:05:10 +0800

    Created GodCard abstract class - created abstract GodCard class to represent the available god cards in the Santorini game. - added name and description variables. - added constructor for GodCard class. - added getter method for name and description. - added boolean canMoveTwice and canBuildTwice.

---
+++ src/GodCard.java
@@ -0,0 +1,24 @@
+public abstract class GodCard {
+    protected String name;
+    protected String description;
+    public GodCard(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean canMoveTwice(){
+        return false;
+    }
+
+    public boolean canBuildTwice(){
+        return false;
+    }
+}
