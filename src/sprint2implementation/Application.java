fd6a9e02f97fc412ec9abf66b79f54137177c4ad
Peen08 <yee0001@student.monash.edu>
2025-04-24 18:36:37 +0800

    Reorganised the code in the application class

--- src/sprint2implementation/Application.java
+++ src/sprint2implementation/Application.java
@@ -6,10 +6,42 @@ import java.util.Collections;
 import java.util.List;
 
 
-public class Application
-{
-    public static void main(String[] args)
+public class Application {
+    public static List<GodCard> instantiateGodCards() {
+        // instantiates the type of god cards
+        GodCard demeter = new Demeter();
+        GodCard artemis = new Artemis();
+
+        // Created a list to store all the god cards
+        List<GodCard> godCards = new ArrayList<>();
+        godCards.add(demeter);
+        godCards.add(artemis);
+
+        // Assign god cards randomly to every players by shuffling them
+        Collections.shuffle(godCards);
+
+        return godCards;
+    }
+
+    public static List<Player> instantiatePlayers(int playerNum, int workerNum, List<GodCard> godCardList)
     {
+        List<Player> playerList = new ArrayList<>();
+
+        for (int playerNo = 0; playerNo < playerNum; playerNo++)
+        {
+            Player player = new Player("Player "+(playerNo+1), "src/sprint2implementation/pics/player_"+(playerNo+1)+".png", workerNum);
+            player.setGodCard(godCardList.get(playerNo));
+            for (int towerNo = 1; towerNo < 4 ; towerNo++)
+            {
+                player.setPlayerPositionTower(towerNo, "src/sprint2implementation/pics/Lvl_"+towerNo+"_player_"+playerNo+".png");
+            }
+            // store player into the player list
+            playerList.add(player);
+        }
+        return playerList;
+    }
+
+    public static void main(String[] args) {
         // instantiate the window object
         Window windowObj = new Window();
         JFrame windowFrame = windowObj.getNewFrame();
@@ -26,11 +58,11 @@ public class Application
         layeredPane.add(bgLabel, JLayeredPane.DEFAULT_LAYER);
 
         // Create a game board (a layer higher)
-        Board gameBoard = new Board(600,5,5);
+        Board gameBoard = new Board(600, 5, 5);
         int boardSize = gameBoard.getBoardSize();
-        int width = (screenSize.width - boardSize)/2;
-        int height = (screenSize.height - boardSize)/2;
-        gameBoard.setBounds(width,height, boardSize, boardSize);
+        int width = (screenSize.width - boardSize) / 2;
+        int height = (screenSize.height - boardSize) / 2;
+        gameBoard.setBounds(width, height, boardSize, boardSize);
         layeredPane.add(gameBoard, JLayeredPane.PALETTE_LAYER);
 
         // Create player's turn label
@@ -40,36 +72,12 @@ public class Application
         turnLabel.setBounds(20, 20, 600, 30);
         layeredPane.add(turnLabel, JLayeredPane.MODAL_LAYER);
 
-        // instantiates the type of god cards
-        GodCard demeter = new Demeter();
-        GodCard artemis = new Artemis();
+        List<GodCard> godCardList = instantiateGodCards();
+        List<Player> playerList = instantiatePlayers(2, 2, godCardList);
 
-        // Created a list to store all the god cards
-        List<GodCard> godCards = new ArrayList<>();
-        godCards.add(demeter);
-        godCards.add(artemis);
-
-        // Assign god cards randomly to every players by shuffling them
-        Collections.shuffle(godCards);
-
-        // Add Players into the game
-        Player player1 = new Player("Player 1", "src/sprint2implementation/pics/player_1.png", 2);
-        Player player2 = new Player("Player 2", "src/sprint2implementation/pics/player_2.png", 2);
-
-        // Add God Card to Players randomly
-        player1.setGodCard(godCards.get(0));
-        player2.setGodCard(godCards.get(1));
-
-        player1.setPlayerPositionTower(1, "src/sprint2implementation/pics/Lvl_1_player_1.png");
-        player1.setPlayerPositionTower(2, "src/sprint2implementation/pics/Lvl_2_player_1.png");
-        player1.setPlayerPositionTower(3, "src/sprint2implementation/pics/Lvl_3_player_1.png");
-
-        player2.setPlayerPositionTower(1, "src/sprint2implementation/pics/Lvl_1_player_2.png");
-        player2.setPlayerPositionTower(2, "src/sprint2implementation/pics/Lvl_2_player_2.png");
-        player2.setPlayerPositionTower(3, "src/sprint2implementation/pics/Lvl_3_player_2.png");
-
-        GameController controller = new GameController(gameBoard, player1, player2, turnLabel);
+        GameController controller = new GameController(gameBoard, playerList, turnLabel);
         windowFrame.addComponentListener(new ResizeListener(windowFrame, gameBoard, boardSize, boardSize));
         windowFrame.setVisible(true);
     }
+
 }
