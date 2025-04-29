839d6893d27c45a7442e4cfedc7d5215a5ce7c8b
Peen08 <yee0001@student.monash.edu>
2025-04-30 05:11:52 +0800

    Reimplement the logic of the worker action (determine the regular actions/ actions with special ability)

--- src/sprint2implementation/characters/Worker.java
+++ src/sprint2implementation/characters/Worker.java
@@ -1,10 +1,17 @@
 package sprint2implementation.characters;
 
+import sprint2implementation.actions.BuildAction;
+import sprint2implementation.actions.MoveAction;
+import sprint2implementation.grounds.Tile;
+import sprint2implementation.towers.Tower;
+
 public class Worker {
     private int row;
     private int col;
     private Player player;
     private boolean booleanStuck;
+    private Boolean isMoving = false;
+
 
     public Worker(Player player) {
         this.player = player;
@@ -45,6 +52,33 @@ public class Worker {
 
     public void setBooleanStuck(boolean booleanStuck) {
         this.booleanStuck = booleanStuck;
+    }
+
+
+    public boolean executeAction(Player currentPlayer, Tile selectedTile, Tile clickedTile, Tower tower)
+    {
+        // execution of regular move and build actions
+        if (currentPlayer.getGodCard() == null)
+        {
+            MoveAction moveAction = new MoveAction(selectedTile, clickedTile, this);
+            moveAction.execute();
+
+            if (moveAction.isMoveSuccessful())
+            {
+                isMoving = true;
+                BuildAction buildAction = new BuildAction(clickedTile, tower);
+                buildAction.execute();
+
+                if (buildAction.isBuildSuccessful())
+                {
+                    currentPlayer.setActionSuccessful(true);
+                }
+            }
+        }
+        else { // special god cards execution of their special abilities
+            isMoving = currentPlayer.getGodCard().executeSpecialAbility(currentPlayer, selectedTile, clickedTile, tower);
+        }
+        return isMoving;
     }
 }
 
