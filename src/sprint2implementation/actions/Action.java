dd6d1cb49922fd44dda3da762ca95d607e2ff391
Peen08 <yee0001@student.monash.edu>
2025-04-30 05:07:03 +0800

    Redo the Action class

--- src/sprint2implementation/actions/Action.java
+++ src/sprint2implementation/actions/Action.java
@@ -5,10 +5,12 @@ public abstract class Action {
     protected int row;
     protected int column;
 
+
     public Action( int row, int column) {
         this.row = row;
         this.column = column;
-
     }
+
     public abstract void execute();
+
 }
