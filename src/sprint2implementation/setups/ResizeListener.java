73b88b059f95e892b2edf66f6336f35653764fdd
Peen08 <yee0001@student.monash.edu>
2025-04-30 05:13:21 +0800

    Apply Singleton design pattern to the ResizeListener class

--- src/sprint2implementation/setups/ResizeListener.java
+++ src/sprint2implementation/setups/ResizeListener.java
@@ -4,18 +4,28 @@ import javax.swing.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
+// Design pattern: Singleton.
+// Singleton is used as the design pattern for the ResizeListener class because only one instance is needed for the window to be resized.
 public class ResizeListener extends ComponentAdapter {
 
+    private static ResizeListener instance;
     private JFrame windowFrame;
     private JComponent gameBoard;
     private int boardWidth;
     private int boardHeight;
 
-    public ResizeListener(JFrame windowFrame, JComponent gameBoard, int width, int height){
+    private ResizeListener(JFrame windowFrame, JComponent gameBoard, int width, int height){
         setWindowFrame(windowFrame);
         setWindowComponent(gameBoard);
         setWidth(width);
         setHeight(height);
+    }
+
+    public static ResizeListener getInstance(JFrame windowFrame, JComponent gameBoard, int width, int height) {
+        if (instance == null) {
+            instance = new ResizeListener(windowFrame, gameBoard, width, height);
+        }
+        return instance;
     }
 
     @Override
