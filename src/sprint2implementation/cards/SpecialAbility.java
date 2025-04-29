76d182819f71fa32031851ba5348f222af6492e0
Peen08 <yee0001@student.monash.edu>
2025-04-30 05:09:36 +0800

    Implement the Strategy method as the design pattern by using the SpecialAbility interface

---
+++ src/sprint2implementation/cards/SpecialAbility.java
@@ -0,0 +1,10 @@
+package sprint2implementation.cards;
+
+import sprint2implementation.characters.Player;
+import sprint2implementation.grounds.Tile;
+import sprint2implementation.towers.Tower;
+
+
+public interface SpecialAbility {
+    boolean executeSpecialAbility(Player currentPlayer, Tile selectedTile, Tile clieckedTile, Tower tower);
+}
