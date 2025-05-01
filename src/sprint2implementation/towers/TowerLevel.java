6ae23a6812bf990746ba6b370376b404385a8236
xwon0021 <xwon0021@student.monash.edu>
2025-05-02 01:37:48 +0800

    Added javadoc for TowerLevel class

--- src/sprint2implementation/towers/TowerLevel.java
+++ src/sprint2implementation/towers/TowerLevel.java
@@ -3,15 +3,35 @@ package sprint2implementation.towers;
 import javax.swing.*;
 import java.net.URL;
 
-public class TowerLevel
-{
+/**
+ * Represents a visual level component of a tower in the game.
+ * Each level is associated with an icon used for display purposes.
+ *
+ * @author Tiffany
+ * Modified by: Yee Peen
+ */
+public class TowerLevel {
+
+    /**
+     * The icon representing this tower level.
+     */
     private ImageIcon levelIcon;
 
-    public TowerLevel(URL iconPath)
-    {
+    /**
+     * Constructs a TowerLevel with the specified image icon path.
+     *
+     * @param iconPath the URL pointing to the image representing this level
+     */
+    public TowerLevel(URL iconPath) {
+
         this.levelIcon = new ImageIcon(iconPath);
     }
 
+    /**
+     * Returns the icon for this tower level.
+     *
+     * @return the ImageIcon representing this level
+     */
     public ImageIcon getLevelIcon() {
         return levelIcon;
     }
