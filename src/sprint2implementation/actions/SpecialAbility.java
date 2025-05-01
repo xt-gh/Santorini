068802a2a389d2a3e6fb9fff690034a550fd28a0
tlau0015 <tlau0015@student.monash.edu>
2025-05-01 11:45:03 +0800

    Deleted the SpecialAbility interface in actions package

--- src/sprint2implementation/actions/SpecialAbility.java
+++
@@ -1,7 +0,0 @@
-package sprint2implementation.actions;
-
-import sprint2implementation.main.GameController;
-
-public interface SpecialAbility {
-    void executeSpecialAbility(GameController gameController);
-}
