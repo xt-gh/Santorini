b9c975a12f31f710887047e4e1cabff3aef7ae93
xwon0021 <xwon0021@student.monash.edu>
2025-05-02 03:13:28 +0800

    Added javadoc for Window class

--- src/sprint2implementation/setups/Window.java
+++ src/sprint2implementation/setups/Window.java
@@ -6,12 +6,34 @@ import java.net.URL;
 
 // Design pattern: Singleton.
 // Singleton is used as the design pattern for the Window class because only one window instance is needed to set up the game.
-public class Window
-{
+
+/**
+ * Singleton class responsible for setting up and managing the main game window.
+ * Design Pattern: Singleton - only one instance of the Window class is needed to control the game window.
+ *
+ * @author Yee Peen
+ * Modified by: Tiffany
+ */
+public class Window {
+
+    /**
+     * The single instance of the Window class
+     */
     private static Window instance;
+
+    /**
+     * The main JFrame used as the game window.
+     */
     private JFrame newFrame;
+
+    /**
+     * The dimensions of the user's screen.
+     */
     private Dimension currentScreenSize;
 
+    /**
+     * Private constructor that initializes the main window with appropriate size, title, and icon.
+     */
     private Window() {
         // Create a main window for Java GUI
         newFrame = new JFrame();
@@ -27,6 +49,11 @@ public class Window
         newFrame.setIconImage(applicationIcon.getImage());
     }
 
+    /**
+     * Returns the singleton instance of the Window class, creating it if necessary.
+     *
+     * @return the single Window instance
+     */
     public static Window getInstance() {
         if (instance == null) {
             instance = new Window();
@@ -34,6 +61,12 @@ public class Window
         return instance;
     }
 
+    /**
+     * Creates and returns a JLabel with the scaled background image set to the screen size.
+     *
+     * @param currentScreenSize the size to scale the background image to
+     * @return a JLabel containing the scaled background image
+     */
     public JLabel setBgLabel(Dimension currentScreenSize)
     {
         // Scale the background and set the background of the frame
@@ -45,10 +78,20 @@ public class Window
         return bgLabel;
     }
 
+    /**
+     * Gets the current screen size of the user's display.
+     *
+     * @return the screen size as a Dimension object
+     */
     public Dimension getCurrentScreenSize() {
         return Toolkit.getDefaultToolkit().getScreenSize();
     }
 
+    /**
+     * Gets the main JFrame representing the game window.
+     *
+     * @return the game window frame
+     */
     public JFrame getNewFrame() {
         return newFrame;
     }
