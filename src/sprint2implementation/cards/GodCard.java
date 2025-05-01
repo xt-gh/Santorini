fa7347aa9b6197e7ffcc164556fbb94a8b427d67
xwon0021 <xwon0021@student.monash.edu>
2025-05-02 02:42:26 +0800

    Added javadoc for GodCard class

--- src/sprint2implementation/cards/GodCard.java
+++ src/sprint2implementation/cards/GodCard.java
@@ -1,18 +1,49 @@
 package sprint2implementation.cards;
 
+/**
+ * Abstract base class representing a God Card in the game.
+ * Each God Card has a name and a description and implements a special ability.
+ *
+ * @author Emil
+ * Modified by: Yee Peen
+ */
 public abstract class GodCard implements SpecialAbility{
+
+    /**
+     * The name of the god represented by the card.
+     */
     protected String name;
+
+    /**
+     * A short description of the god's special ability.
+     */
     protected String description;
 
+    /**
+     * Constructor of GodCard.
+     *
+     * @param name the name of teh god
+     * @param description the description of the god's special ability
+     */
     public GodCard(String name, String description){
         this.name = name;
         this.description = description;
     }
 
+    /**
+     * Returns the name of the god.
+     *
+     * @return the name of the god.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the description of the god's special ability.
+     *
+     * @return the description of the special ability
+     */
     public String getDescription() {
         return description;
     }
