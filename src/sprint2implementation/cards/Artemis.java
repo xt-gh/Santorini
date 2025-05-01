0022404d31313ea48f034463dc96d3c136754060
xwon0021 <xwon0021@student.monash.edu>
2025-05-02 03:07:11 +0800

    Added javadoc for Artemis class

--- src/sprint2implementation/cards/Artemis.java
+++ src/sprint2implementation/cards/Artemis.java
@@ -8,18 +8,63 @@ import sprint2implementation.towers.Tower;
 
 import javax.swing.*;
 
+/**
+ * Represents the Artemis God card which allows a player to move twice in one turn,
+ * but cannot return to the original tile.
+ *
+ * @author Tiffany
+ * Modified by: Yee Peen, Emil
+ */
 public class Artemis extends GodCard {
+
+    /**
+     * The tile from which the first move was made.
+     */
     private Tile firstMoveFromTile = null;
+
+    /**
+     * Indicates whether the player is waiting to perform the second move.
+     */
     private boolean waitingForSecondMove = false;
+
+    /**
+     * Indicates whether the building phase is active.
+     */
     private boolean isBuildingPhase = false;
+
+    /**
+     * Indicates whether the player is in the moving phase.
+     */
     private boolean isMoving = false;
+
+    /**
+     * The maximum number of moves allowed (2 for Artemis).
+     */
     private final int MAX_MOVE = 2;
+
+    /**
+     * The number of moves performed so far.
+     */
     private int movedNum = 0;
 
+    /**
+     * Constructs an Artemis GodCard with name and description.
+     */
     public Artemis(){
         super("Artemis", "Artemis can move twice in a turn.");
     }
 
+    /**
+     * Executes Artemis's special ability, allowing up to two moves followed by a build.
+     * The second move cannot be back to the original tile.
+     *
+     * @param currentPlayer the player currently performing the action
+     * @param selectedTile the tile where the current worker starts (or is standing)
+     * @param clickedTile the tile the player chooses to move to or build on
+     * @param tower the tower to build upon during the building phase
+     *
+     * @return true if still in the moving phase, false otherwise
+     */
     public boolean executeSpecialAbility(Player currentPlayer, Tile selectedTile, Tile clickedTile, Tower tower) {
         if (!isBuildingPhase) {
             if (clickedTile == firstMoveFromTile) { // Can't move back to original position
