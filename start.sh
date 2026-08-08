3b0910ddf93b7454d78d36bb59c459ac5e6efbae
xwon0021 <xwon0021@student.monash.edu>
2026-08-09 01:03:01 +0800

    Fix Render URL to open directly

--- start.sh
+++ start.sh
@@ -12,6 +12,9 @@ openbox-session &
 # Start VNC server
 x11vnc -display :0 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever -shared &
 
+# Make vnc.html the default page so it opens directly
+ln -s /usr/share/novnc/vnc.html /usr/share/novnc/index.html
+
 # Start noVNC web server on port $PORT (which Render will expose)
 websockify --web /usr/share/novnc/ ${PORT:-10000} localhost:5900 &
 
